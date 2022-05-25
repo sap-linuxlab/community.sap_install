a896b174c14fed5639430928fbb6e51bf107e8e2
Bernd Finger <>
2022-05-25 17:06:20 +0200

    sap_hana_install: skip one more shellcheck check

--- roles/sap_hana_install/files/tmp/tail-f-hdblcm-install-trc.sh
+++ roles/sap_hana_install/files/tmp/tail-f-hdblcm-install-trc.sh
@@ -5,8 +5,10 @@ while true; do
    ps -ef | awk '/\/usr\/sap\/hostctrl\/exe\/sapdsigner/&&!/awk/{print}'
    _HDBLCM_PID=$(ps -ef | awk '/hdblcm/&&/instlog_dir/&&!/awk/{print $2}')
    if [[ ${_HDBLCM_PID}. != "." ]]; then
-# apparently a wrong catch by shellcheck: We are using braces inside awk, not inside the shell.
-# shellcheck disable=SC2125
+# skip SC2046: No need to quote at "$(ps" because the awk statement should take
+#                care of preventing word splitting
+# skip SC2125: We are using braces inside awk, not inside the shell.
+# shellcheck disable=SC2046,SC2125
       _HDBLCM_TRC_FILE=$(echo /var/tmp/hdblcm_$(ps -ef | awk '/hdblcm/{print}' | \
         awk 'BEGIN{RS=" "}/instlog_dir/{split ($0, a, "install_"); print a[2]}')*.trc)
       echo "hdblcm trace file: ${_HDBLCM_TRC_FILE}"
