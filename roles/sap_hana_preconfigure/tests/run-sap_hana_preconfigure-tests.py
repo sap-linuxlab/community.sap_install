7838aaaab46eb93414f3365c835a8c657dea0f90
Bernd Finger <bfinger@redhat.com>
2022-07-01 17:10:36 +0200

    preconfigure roles: Solve pep8 errors

--- roles/sap_hana_preconfigure/tests/run-sap_hana_preconfigure-tests.py
+++ roles/sap_hana_preconfigure/tests/run-sap_hana_preconfigure-tests.py
@@ -12,17 +12,18 @@ import subprocess
 FIELD_DELIMITER = '\t'
 
 if len(sys.argv) == 1:
-    _managed_node=input("Provide name of managed node: ")
+    _managed_node = input("Provide name of managed node: ")
 else:
-    _managed_node=sys.argv[1]
+    _managed_node = sys.argv[1]
 
 print('Running tests for role sap_hana_preconfigure...\n')
 print('Managed node: ' + _managed_node)
 
-_mn_rhel_release = subprocess.getoutput("ssh root@"
-  + _managed_node
-  + " cat /etc/redhat-release | \
-    awk 'BEGIN{FS=\"release \"}{split ($2, a, \" \"); print a[1]}'")
+_mn_rhel_release = subprocess.getoutput(
+    "ssh root@"
+    + _managed_node
+    + " cat /etc/redhat-release | \
+      awk 'BEGIN{FS=\"release \"}{split($2, a, \" \"); print a[1]}'")
 print('Managed node Red Hat release: ' + _mn_rhel_release)
 _mn_hw_arch = subprocess.getoutput("ssh root@" + _managed_node + " uname -m")
 print('Managed node HW architecture: ' + _mn_hw_arch)
@@ -264,7 +265,7 @@ __tests = [
 ]
 
 for par1 in __tests:
-    print ('\n' + 'Test ' + par1['number'] + ': ' + par1['name'])
+    print('\n' + 'Test ' + par1['number'] + ': ' + par1['name'])
     command = ('ansible-playbook sap_hana_preconfigure-default-settings.yml '
                + par1['command_line_parameter']
                + '-l '
@@ -276,48 +277,48 @@ for par1 in __tests:
     command += '"'
     if par1['compact_assert_output']:
         command += ' | ../tools/beautify-assert-output.sh'
-    print ("command: " + command)
+    print("command: " + command)
     _py_rc = os.system(command)
-    par1['rc'] = str(int(_py_rc/256))
+    par1['rc'] = str(int(_py_rc / 256))
     if _py_rc != 0:
         if par1['ignore_error_final']:
             print('Test '
-              + par1['number']
-              + ' finished with return code '
-              + par1['rc']
-              + '. Continuing with the next test')
+                  + par1['number']
+                  + ' finished with return code '
+                  + par1['rc']
+                  + '. Continuing with the next test')
         else:
             print('Test '
-              + par1['number']
-              + ' finished with return code '
-              + par1['rc'] + '.')
+                  + par1['number']
+                  + ' finished with return code '
+                  + par1['rc'] + '.')
             sys.exit(_py_rc)
     else:
         print('Test ' + par1['number'] + ' finished with return code ' + par1['rc'] + '.')
 
-print ('\nResults for role sap_hana_preconfigure: '
-  + _managed_node
-  + ' - RHEL '
-  + _mn_rhel_release
-  + ' - '
-  + _mn_hw_arch + ':')
+print('\nResults for role sap_hana_preconfigure: '
+      + _managed_node
+      + ' - RHEL '
+      + _mn_rhel_release
+      + ' - '
+      + _mn_hw_arch + ':')
 
-print ('\n#'
-       + FIELD_DELIMITER
-       + 'RC' + FIELD_DELIMITER
-       + 'name' + FIELD_DELIMITER
-       + 'argument' + FIELD_DELIMITER
-       + 'compact' + FIELD_DELIMITER
-       + 'role_vars')
+print('\n#'
+      + FIELD_DELIMITER
+      + 'RC' + FIELD_DELIMITER
+      + 'name' + FIELD_DELIMITER
+      + 'argument' + FIELD_DELIMITER
+      + 'compact' + FIELD_DELIMITER
+      + 'role_vars')
 
 for par1 in __tests:
-    print (par1['number'] + FIELD_DELIMITER
-           + par1['rc'] + FIELD_DELIMITER
-           + par1['name'] + FIELD_DELIMITER
-           + par1['command_line_parameter'] + FIELD_DELIMITER
-           + str(par1['compact_assert_output']) + FIELD_DELIMITER, end='')
+    print(par1['number'] + FIELD_DELIMITER
+          + par1['rc'] + FIELD_DELIMITER
+          + par1['name'] + FIELD_DELIMITER
+          + par1['command_line_parameter'] + FIELD_DELIMITER
+          + str(par1['compact_assert_output']) + FIELD_DELIMITER, end='')
     if len(par1['role_vars']) == 0:
-        print ("")
+        print("")
     else:
         for par2 in par1['role_vars']:
-            print (str(par2))
+            print(str(par2))
