2e04a3630fcb9e00129ec5bb596437f9fa4be568
Bernd Finger <bfinger@redhat.com>
2025-08-13 11:49:39 +0200

    sap_swpm: Implement a test and a filter for validating hostnames
    The related SAP note is 611361 and it defines the criteria for valid
    hostnames for SAP ABAP Platform systems.
    
    Solves issue #1104.
    
    Signed-off-by: Bernd Finger <bfinger@redhat.com>

---
+++ plugins/test/valid_sap_abap_platform_hostname.py
@@ -0,0 +1,71 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = """
+    name: valid_sap_abap_platform_hostname
+    short_description: Test if a string is a valid SAP hostname
+    description:
+        - Tests if a string meets hostname validation criteria
+        - Checks if it is a string, has 13 characters or less, does not start with a number,
+          only contains alpha characters, digits and the hyphen character, and does not contain a dot character
+    options:
+        _input:
+            description: The string to test
+            type: string
+            required: true
+"""
+
+EXAMPLES = """
+# Test if variable is a valid SAP hostname
+- assert:
+    that:
+      - my_hostname is community.sap_install.valid_sap_abap_platform_hostname
+    fail_msg: "{{ my_hostname }} is not a valid hostname"
+
+# In a conditional
+- debug:
+    msg: "Valid hostname"
+  when: server_name is community.sap_install.valid_sap_abap_platform_hostname
+"""
+
+RETURN = """
+_result:
+    description: True if the string meets all hostname criteria, False otherwise
+    type: bool
+"""
+
+
+def valid_sap_abap_platform_hostname(value):
+    """
+    Test if value is a valid hostname according to specified criteria:
+    1. Must be a string
+    2. Must be 13 characters or less
+    3. Must not start with a number
+    4. Must not contain a dot
+    5. Only contains alpha characters, digits and hyphen characters
+
+    Returns True if valid, False if invalid
+    """
+    if not isinstance(value, str):
+        return False
+
+    if len(value) > 13:
+        return False
+
+    if value and value[0].isdigit():
+        return False
+
+    if value and not value.replace('-', 'x').isalnum():
+        return False
+
+    if value and str(value).find('.') != -1:
+        return False
+
+    return True
+
+
+class TestModule(object):
+    def tests(self):
+        return {
+            'valid_sap_abap_platform_hostname': valid_sap_abap_platform_hostname,
+        }
